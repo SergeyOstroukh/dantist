9c9855d91241ed557018d2ee08b60b1fb041fb55
hardmass <hardmass3ffect@gmail.com>
2025-04-01 14:06:04 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
 import Header from "@/components/header"
-import { Footer } from "@/components/footer"
-import { Services } from "@/components/Services"
-        import Main from "@/components/main";
+import {Footer} from "@/components/footer"
+import {Services} from "@/components/Services"
+import Main from "@/components/main";
 
 
 export default function Page() {
-  return (
-    <div className="flex flex-col md:justify-between w-full h-screen">
-      <Header />
-                  <Main/>
-
-      <Services />
-      <Footer />
-    </div>
-  )
+    return (
+        <div className="flex flex-col md:justify-between w-full h-screen">
+            <Header/>
+            <Main/>
+            <Services/>
+            <Footer/>
+        </div>
+    )
 }
