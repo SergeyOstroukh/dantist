7a9f536f247dbace70b8007866801ea4113861c5
gem4ik <mgemskij@gmail.com>
2025-04-01 16:18:54 +0300

    add bg-image

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,45 +1,29 @@
 "use client"
-import React, {useEffect, useState} from 'react';
-import Image from "next/image";
-import banner360 from "../images/banner360.svg"
-import mainBanner from "../images/MainBaner.svg"
-import {Button} from "@/components/shared/Button";
+import React from "react"
+import { Button } from "@/components/shared/Button"
 
 const Main = () => {
-        const [isMdScreen, setIsMdScreen] = useState(false);
-
-        useEffect(() => {
-            const handleResize = () => {
-                setIsMdScreen(window.innerWidth >= 768); // 768px — это значение для md в Tailwind CSS
-            };
-
-            handleResize(); // Установим начальное значение
-            window.addEventListener('resize', handleResize);
-
-            return () => {
-                window.removeEventListener('resize', handleResize);
-            };
-        }, []);
-    return (
-        <div className='flex justify-end w-full min-w-[300px]'>
-            <div className='relative'>
-                <Image
-                    src={isMdScreen ? mainBanner : banner360}
-                    alt='banner'
-                    width={isMdScreen ? 768 : 320}
-                    height={isMdScreen ? 440 : 440}
-                />
-            </div>
-            <div className='flex flex-col absolute p-4 gap-5 items-start left-0 md:justify-start'>
-                <div className='font-bold text-xl max-w-[400px]'>Лечение и протезирование зубов по Европейским стандартам</div>
-                <div className='text-xl text-[#099DBA] max-w-[400px]'>Предоставим 2-3 плана качественного лечения под ваш бюджет</div>
-                <div className='font-light max-w-[400px]'>Запишитесь на <span className='text-[#099DBA]'>бесплатную</span> первичную консультацию - диагностику зубов</div>
-                <div className='w-full flex justify-center items-center'>
-                    <Button>Записаться</Button>
-                </div>
-            </div>
+  return (
+    <div className="flex justify-end w-full min-w-[300px]">
+      <div className="flex flex-col sm:bg-cover bg-right p-4 gap-5 w-full bg-[url('../images/banner360.svg')] sm:bg-[url('../images/MainBaner.svg')] sm:bg-repeat sm:w-full bg-no-repeat items-start left-0 md:justify-start">
+        <div>
+          <div className="font-bold text-xl max-w-[400px]">
+            Лечение и протезирование зубов по Европейским стандартам
+          </div>
+          <div className="text-xl text-[#099DBA] max-w-[400px]">
+            Предоставим 2-3 плана качественного лечения под ваш бюджет
+          </div>
+          <div className="font-light max-w-[400px]">
+            Запишитесь на <span className="text-[#099DBA]">бесплатную</span>{" "}
+            первичную консультацию - диагностику зубов
+          </div>
         </div>
-    );
-};
+        <div className="w-full flex justify-center items-center">
+          <Button>Записаться</Button>
+        </div>
+      </div>
+    </div>
+  )
+}
 
-export default Main;
+export default Main
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,216 +1,219 @@
-import plugin from 'tailwindcss/plugin'
-
+import plugin from "tailwindcss/plugin"
 
 const colors = {
-    accent: {
-        100: '#73A5FF',
-        300: '#4C8DFF',
-        500: '#397DF6',
-        700: '#2F68CC',
-        900: '#234E99',
-    },
-    danger: {
-        50: '#FF809980',
-        100: '#ff8099',
-        300: '#F23D61',
-        500: '#CC1439',
-        700: '#990F2B',
-        900: '#660A1D',
-    },
-    dark: {
-        50: '#4C4C4C80',
-        100: '#4C4C4C',
-        300: '#333333',
-        500: '#171717',
-        700: '#0D0D0D',
-        900: '#000000',
-    },
-    light: {
-        100: '#FFFFFF',
-        300: '#F7FBFF',
-        500: '#EDF3FA',
-        700: '#D5DAE0',
-        900: '#8D9094',
-    },
-    success: {
-        100: '#80FFBF',
-        300: '#22E584',
-        500: '#14CC70',
-        700: '#0F9954',
-        900: '#0A6638',
-    },
-    warning: {
-        50: '#FFD07380',
-        100: '#FFD073',
-        300: '#E5AC39',
-        500: '#D99000',
-        700: '#996600',
-        900: '#664400',
-    },
+  accent: {
+    100: "#73A5FF",
+    300: "#4C8DFF",
+    500: "#397DF6",
+    700: "#2F68CC",
+    900: "#234E99",
+  },
+  danger: {
+    50: "#FF809980",
+    100: "#ff8099",
+    300: "#F23D61",
+    500: "#CC1439",
+    700: "#990F2B",
+    900: "#660A1D",
+  },
+  dark: {
+    50: "#4C4C4C80",
+    100: "#4C4C4C",
+    300: "#333333",
+    500: "#171717",
+    700: "#0D0D0D",
+    900: "#000000",
+  },
+  light: {
+    100: "#FFFFFF",
+    300: "#F7FBFF",
+    500: "#EDF3FA",
+    700: "#D5DAE0",
+    900: "#8D9094",
+  },
+  success: {
+    100: "#80FFBF",
+    300: "#22E584",
+    500: "#14CC70",
+    700: "#0F9954",
+    900: "#0A6638",
+  },
+  warning: {
+    50: "#FFD07380",
+    100: "#FFD073",
+    300: "#E5AC39",
+    500: "#D99000",
+    700: "#996600",
+    900: "#664400",
+  },
 }
 
 module.exports = {
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            backgroundColor: {
-                ...colors,
-            },
-            borderColor: {
-                ...colors,
-            },
-            borderRadius: {
-                lg: 'var(--radius)',
-                md: 'calc(var(--radius) - 2px)',
-                sm: 'calc(var(--radius) - 4px)',
-            },
-            colors: {
-                accent: {
-                    DEFAULT: 'hsl(var(--accent))',
-                    foreground: 'hsl(var(--accent-foreground))',
-                },
-                background: 'hsl(var(--background))',
-                border: 'hsl(var(--border))',
-                card: {
-                    DEFAULT: 'hsl(var(--card))',
-                    foreground: 'hsl(var(--card-foreground))',
-                },
-                chart: {
-                    '1': 'hsl(var(--chart-1))',
-                    '2': 'hsl(var(--chart-2))',
-                    '3': 'hsl(var(--chart-3))',
-                    '4': 'hsl(var(--chart-4))',
-                    '5': 'hsl(var(--chart-5))',
-                },
-                danger: 'colors.danger',
-                dark: 'colors.dark',
-                destructive: {
-                    DEFAULT: 'hsl(var(--destructive))',
-                    foreground: 'hsl(var(--destructive-foreground))',
-                },
-                foreground: 'hsl(var(--foreground))',
-                input: 'hsl(var(--input))',
-                light: 'colors.light',
-                muted: {
-                    DEFAULT: 'hsl(var(--muted))',
-                    foreground: 'hsl(var(--muted-foreground))',
-                },
-                popover: {
-                    DEFAULT: 'hsl(var(--popover))',
-                    foreground: 'hsl(var(--popover-foreground))',
-                },
-                primary: {
-                    DEFAULT: 'hsl(var(--primary))',
-                    foreground: 'hsl(var(--primary-foreground))',
-                },
-                ring: 'hsl(var(--ring))',
-                secondary: {
-                    DEFAULT: 'hsl(var(--secondary))',
-                    foreground: 'hsl(var(--secondary-foreground))',
-                },
-                success: 'colors.success',
-                warning: 'colors.warning',
-            },
-            gridTemplateColumns: {
-                posts: 'repeat(auto-fill, minmax(200px, 1fr))',
-            },
-            height: {
-                headerHeight: 'calc(100vh - 60px)',
-            },
-            textColor: {
-                ...colors,
-            },
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundColor: {
+        ...colors,
+      },
+      backgroundImage: {
+        "main-banner-360": "url('/images/banner360.svg')",
+        "main-banner-768": "url('/images/MainBaner.svg')",
+      },
+      borderColor: {
+        ...colors,
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
         },
-        fontFamily: {
-            sans: ['Inter', 'sans-serif'],
+        background: "hsl(var(--background))",
+        border: "hsl(var(--border))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
         },
-        screens: {
-            lg: '1024px',
-            md: '768px',
-            ml: '425px',
-            sm: '480px',
-            xl: '1440px',
+        chart: {
+          1: "hsl(var(--chart-1))",
+          2: "hsl(var(--chart-2))",
+          3: "hsl(var(--chart-3))",
+          4: "hsl(var(--chart-4))",
+          5: "hsl(var(--chart-5))",
         },
+        danger: "colors.danger",
+        dark: "colors.dark",
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        foreground: "hsl(var(--foreground))",
+        input: "hsl(var(--input))",
+        light: "colors.light",
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        ring: "hsl(var(--ring))",
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        success: "colors.success",
+        warning: "colors.warning",
+      },
+      gridTemplateColumns: {
+        posts: "repeat(auto-fill, minmax(200px, 1fr))",
+      },
+      height: {
+        headerHeight: "calc(100vh - 60px)",
+      },
+      textColor: {
+        ...colors,
+      },
     },
-    variants: {
-        extend: {},
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
     },
-    plugins: [
-        plugin(function ({ addUtilities }) {
-            const newUtilities = {
-                '.text-bold-14': {
-                    fontSize: '14px',
-                    fontWeight: '700',
-                    lineHeight: '24px',
-                },
-                '.text-bold-16': {
-                    fontSize: '16px',
-                    fontWeight: '700',
-                    lineHeight: '24px',
-                },
-                '.text-h1': {
-                    fontSize: '20px',
-                    fontWeight: '700',
-                    lineHeight: '36px',
-                },
-                '.text-h2': {
-                    fontSize: '18px',
-                    fontWeight: '700',
-                    lineHeight: '24px',
-                },
-                '.text-h3': {
-                    fontSize: '16px',
-                    fontWeight: '600',
-                    lineHeight: '24px',
-                },
-                '.text-large': {
-                    fontSize: '26px',
-                    fontWeight: '600',
-                    lineHeight: '36px',
-                },
-                '.text-medium-14': {
-                    fontSize: '14px',
-                    fontWeight: '500',
-                    lineHeight: '24px',
-                },
-                '.text-regular-14': {
-                    fontSize: '14px',
-                    fontWeight: '400',
-                    lineHeight: '24px',
-                },
-                '.text-regular-16': {
-                    fontSize: '16px',
-                    fontWeight: '400',
-                    lineHeight: '24px',
-                },
-                '.text-regular-link': {
-                    color: colors.accent['500'],
-                    fontSize: '14px',
-                    fontWeight: '400',
-                    lineHeight: '24px',
-                    textDecoration: 'underline',
-                },
-                '.text-semibold-small': {
-                    fontSize: '12px',
-                    fontWeight: '600',
-                    lineHeight: '16px',
-                },
-                '.text-small': {
-                    fontSize: '12px',
-                    fontWeight: '400',
-                    lineHeight: '16px',
-                },
-                '.text-small-link': {
-                    color: colors.accent['500'],
-                    fontSize: '12px',
-                    fontWeight: '400',
-                    lineHeight: '16px',
-                    textDecoration: 'underline',
-                },
-            }
+    screens: {
+      lg: "1024px",
+      md: "768px",
+      ml: "425px",
+      sm: "480px",
+      xl: "1440px",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      const newUtilities = {
+        ".text-bold-14": {
+          fontSize: "14px",
+          fontWeight: "700",
+          lineHeight: "24px",
+        },
+        ".text-bold-16": {
+          fontSize: "16px",
+          fontWeight: "700",
+          lineHeight: "24px",
+        },
+        ".text-h1": {
+          fontSize: "20px",
+          fontWeight: "700",
+          lineHeight: "36px",
+        },
+        ".text-h2": {
+          fontSize: "18px",
+          fontWeight: "700",
+          lineHeight: "24px",
+        },
+        ".text-h3": {
+          fontSize: "16px",
+          fontWeight: "600",
+          lineHeight: "24px",
+        },
+        ".text-large": {
+          fontSize: "26px",
+          fontWeight: "600",
+          lineHeight: "36px",
+        },
+        ".text-medium-14": {
+          fontSize: "14px",
+          fontWeight: "500",
+          lineHeight: "24px",
+        },
+        ".text-regular-14": {
+          fontSize: "14px",
+          fontWeight: "400",
+          lineHeight: "24px",
+        },
+        ".text-regular-16": {
+          fontSize: "16px",
+          fontWeight: "400",
+          lineHeight: "24px",
+        },
+        ".text-regular-link": {
+          color: colors.accent["500"],
+          fontSize: "14px",
+          fontWeight: "400",
+          lineHeight: "24px",
+          textDecoration: "underline",
+        },
+        ".text-semibold-small": {
+          fontSize: "12px",
+          fontWeight: "600",
+          lineHeight: "16px",
+        },
+        ".text-small": {
+          fontSize: "12px",
+          fontWeight: "400",
+          lineHeight: "16px",
+        },
+        ".text-small-link": {
+          color: colors.accent["500"],
+          fontSize: "12px",
+          fontWeight: "400",
+          lineHeight: "16px",
+          textDecoration: "underline",
+        },
+      }
 
-            addUtilities(newUtilities)
-        }),
-    ],
+      addUtilities(newUtilities)
+    }),
+  ],
 }
