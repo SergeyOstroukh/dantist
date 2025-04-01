3708e12eb94a6a4ace838ff6e40a1a572350f685
hardmass <hardmass3ffect@gmail.com>
2025-04-01 14:02:32 +0300

    banner section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/header"
-import { Footer } from "@/components/footer"
+import {Footer} from "@/components/footer"
+import Main from "@/components/main";
 
 export default function Page() {
-  return (
-    <div className="flex flex-col justify-between w-full h-screen">
-      <Header />
-      <Footer />
-    </div>
-  )
+    return (
+        <div className="flex flex-col md:justify-between w-full h-screen">
+            <Header/>
+            <Main/>
+            <Footer/>
+        </div>
+    )
 }
