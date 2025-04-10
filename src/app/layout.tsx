a35f3307a2703a77dcccf2442f81b213b5b482ad
hardmass <hardmass3ffect@gmail.com>
2025-04-10 17:51:46 +0300

    favicon fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,28 @@
-import type { Metadata } from "next"
+import type {Metadata} from "next"
 import "@/app/globals.css"
 import React from "react";
 
 export const metadata: Metadata = {
-  title: "Дантист клиник",
-  description: "Лучшие стоматологические услуги в вашем городе. Профессиональные врачи, современные технологии.",
-  keywords: "дантист, стоматология, стоматологические услуги, лечение зубов",
+    title: "Дантист клиник",
+    description: "Лучшие стоматологические услуги в вашем городе. Профессиональные врачи, современные технологии.",
+    keywords: "дантист, стоматология, стоматологические услуги, лечение зубов",
+    icons: {
+        icon: "/favicon.ico",
+    },
 }
 
 export default function RootLayout({
-                                     children,
+                                       children,
                                    }: Readonly<{
-  children: React.ReactNode
+    children: React.ReactNode
 }>) {
-  return (
-      <html lang="en">
-      <head>
-        <link rel="icon" href="favicon.ico" type="image/png" />
-        <title>{metadata.title}</title>
-      </head>
-      <body>{children}</body>
-      </html>
-  )
+    return (
+        <html lang="en">
+        <head>
+            <link rel="icon" href="favicon.ico" type="image/png"/>
+            <title>{metadata.title}</title>
+        </head>
+        <body>{children}</body>
+        </html>
+    )
 }
