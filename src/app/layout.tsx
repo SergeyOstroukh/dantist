183c177cd4e733964cbf864fe7a660ab4f78a8d8
hardmass <hardmass3ffect@gmail.com>
2025-04-19 09:00:36 +0300

    add meta for yandex

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
         <head>
             <meta charSet="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="yandex-verification" content="26ae3287c0ac46cb" />
             <title>Дантист клиник</title>
             <link rel="icon" href="/favicon.ico" type="image/png" />
         </head>
